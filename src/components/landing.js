997b8e7cb2098084c3da3544fe4f585b842cfd4b
zeka2021 <gerasimovajeka@gmail.com>
2021-11-08 23:33:28 +0200

    add structure of the Landing component

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import restaurants from '../sample-restaurants';
 
 
 class Landing extends React.Component {
     render() {
+       
+
         return (
-            <p>Hello App!</p>
-        )
+           
+            <div className='restaurant_select'>
+                <div className='restaurant_select_top'>
+                    <div className='restaurant_select_top_header'>Выбери ресторан</div>
+                    <div className='arrow_picker'>
+                        <div className='arrow_picker-up'></div>
+                        <div className='arrow_picker-down'></div>
+                    </div>
+                </div>
+
+                <div className='restaurant_select_bottom'>
+                    <ul>
+                         {restaurants.map(restaurant => {
+                             return <li key={restaurant.id}>{restaurant.title}</li>;
+                            })}                     
+                      
+                    </ul>
+                    </div>
+                    <button>Перейти в ресторан</button>
+            </div>
+                
+                
+        );
     }
 }
 export default Landing;
