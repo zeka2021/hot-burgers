937788aa70f700aa215d2490e3dc0ddfa8b75e53
zeka2021 <gerasimovajeka@gmail.com>
2021-11-09 16:04:35 +0200

    add burgers in menu

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from './Header';
 import Order from './Order';
 import MenuAdmin from "./MenuAdmin";
+import sampleBurgers from '../sample-burgers';
 
 class App extends React.Component {
     state = {
@@ -15,6 +16,11 @@ class App extends React.Component {
         this.setState({ burgers });
         
     }
+
+
+    loadSampleBurgers = () => {
+        this.setState({ burgers: sampleBurgers });
+    }
     render() {
         return (
             <div className='burger-paradise'>
@@ -22,7 +28,10 @@ class App extends React.Component {
                     <Header title="Very Hot Burger" amount={10} hot={true}/>
                 </div>
                  <Order />
-                    <MenuAdmin addBurger={this.addBurger} />
+                <MenuAdmin
+                    addBurger={this.addBurger}
+                loadSampleBurgers={this.loadSampleBurgers}
+                />
             </div>
         );
        
--- src/components/MenuAdmin.js
+++ src/components/MenuAdmin.js
@@ -8,6 +8,8 @@ class MenuAdmin extends React.Component {
             <div className='menu-admin'>
                 <h2>Управление Меню</h2>
                 <AddBurgerForm addBurger={this.props.addBurger} />
+                <button onClick={this.props.loadSampleBurgers}>
+                    Загрузить бургеры</button>
             </div>
         );
        
