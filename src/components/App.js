408d6b76745e333fd762d0adbea0e4f402bc3a2d
zeka2021 <gerasimovajeka@gmail.com>
2021-11-09 21:28:25 +0200

    reflects component Order

--- src/components/App.js
+++ src/components/App.js
@@ -44,7 +44,7 @@ class App extends React.Component {
                         })}
                     </ul>
                 </div>
-                 <Order />
+                <Order burgers={this.state.burgers} order={this.state.order}/>
                 <MenuAdmin
                     addBurger={this.addBurger}
                 loadSampleBurgers={this.loadSampleBurgers}
